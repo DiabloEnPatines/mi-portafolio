1687d8a39734e203b87c0c28c8f9f12abc03a688
Ariel Peñaloza R <104028724+DiabloEnPatines@users.noreply.github.com>
2023-08-23 23:13:52 -0400

    sgndo commit

--- mi-portafolio/src/componentes/Contact.js
+++ mi-portafolio/src/componentes/Contact.js
@@ -1,25 +1,43 @@
 import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
+
 import '../App.css';
 
-const Contact = () => {
+
+function Contact() {
+  const [state, handleSubmit] = useForm("https://formspree.io/f/moqodnky");
+  if (state.succeeded) {
+      return <p>Thanks for joining!</p>;
+  }
   return (
-    <div className="contact">
-      <h2>Contacto</h2>
-      <p>Si deseas ponerte en contacto conmigo, no dudes en escribirme:</p>
-      <form action="https://formspree.io/p.rodriguez.ariel1@gmail.com" method="POST">
-        <label for="nombre">Nombre:</label>
-        <input type="text" name="nombre" required />
-
-        <label for="email">Email:</label>
-        <input type="email" name="email" required />
-
-        <label for="mensaje">Mensaje:</label>
-        <textarea name="mensaje" required></textarea>
-
-        <button type="submit">Enviar</button>
-      </form>
-    </div>
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Direccion de Correo
+      </label>
+      <input
+        id="email"
+        type="email" 
+        name="email"
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      <textarea
+        id="message"
+        name="message"
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" disabled={state.submitting}>
+        Enviar
+      </button>
+    </form>
   );
-};
+}
 
 export default Contact;
