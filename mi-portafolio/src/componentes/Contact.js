24d18809cba27fc92b495a8be8d69f04bd707b04
Ariel Peñaloza R <104028724+DiabloEnPatines@users.noreply.github.com>
2023-08-23 22:56:18 -0400

    sgndo commit

--- mi-portafolio/src/componentes/Contact.js
+++ mi-portafolio/src/componentes/Contact.js
@@ -6,19 +6,18 @@ const Contact = () => {
     <div className="contact">
       <h2>Contacto</h2>
       <p>Si deseas ponerte en contacto conmigo, no dudes en escribirme:</p>
-      <form action="enviar_formulario.php" method="POST">
+      <form action="https://formspree.io/p.rodriguez.ariel1@gmail.com" method="POST">
         <label for="nombre">Nombre:</label>
-         <input type="text" name="nombre" required />
+        <input type="text" name="nombre" required />
 
-         <label for="email">Email:</label>
-         <input type="email" name="email" required />
+        <label for="email">Email:</label>
+        <input type="email" name="email" required />
 
-         <label for="mensaje">Mensaje:</label>
+        <label for="mensaje">Mensaje:</label>
         <textarea name="mensaje" required></textarea>
 
         <button type="submit">Enviar</button>
       </form>
-
     </div>
   );
 };
